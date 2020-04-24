fd65cdaf6c4ab19027369498fc67ba7d62c3b1db
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-04-24 16:55:21 +0530

    Updates

---
+++ precache-manifest.2505638a635571b2cab9edb0702fdca5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f5e3853af3b8c27d60f70b6b4248a2",
+    "url": "/chapli-sante/index.html"
+  },
+  {
+    "revision": "342e77e1a0beae715129",
+    "url": "/chapli-sante/static/css/2.51de6d05.chunk.css"
+  },
+  {
+    "revision": "a724175554bb5002b1bd",
+    "url": "/chapli-sante/static/css/main.c55c7688.chunk.css"
+  },
+  {
+    "revision": "342e77e1a0beae715129",
+    "url": "/chapli-sante/static/js/2.42267b2b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/chapli-sante/static/js/2.42267b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a724175554bb5002b1bd",
+    "url": "/chapli-sante/static/js/main.51bd25da.chunk.js"
+  },
+  {
+    "revision": "055f707a85d1df45c5e3",
+    "url": "/chapli-sante/static/js/runtime-main.5d175e0f.js"
+  },
+  {
+    "revision": "1d62e6a4789f6ca2dad604cc7d284c37",
+    "url": "/chapli-sante/static/media/brand-1.1d62e6a4.jpg"
+  },
+  {
+    "revision": "3dc613cbfa861da68e459da88f1a68e1",
+    "url": "/chapli-sante/static/media/brand-2.3dc613cb.jpg"
+  },
+  {
+    "revision": "1cc7855ba0c13450ba5544328d68cb28",
+    "url": "/chapli-sante/static/media/brand-3.1cc7855b.jpg"
+  },
+  {
+    "revision": "85f988ae7c75568f06b429a77f36272a",
+    "url": "/chapli-sante/static/media/brand-4.85f988ae.jpg"
+  },
+  {
+    "revision": "c8a94cc550cbf96832351f2b8cbcec86",
+    "url": "/chapli-sante/static/media/brand-5.c8a94cc5.jpg"
+  },
+  {
+    "revision": "36bc06367e8c5f0bc93965891770d629",
+    "url": "/chapli-sante/static/media/casuals.36bc0636.jpg"
+  },
+  {
+    "revision": "f92c2a78d0899db733e0b81a4168a5c9",
+    "url": "/chapli-sante/static/media/cover-img-1.f92c2a78.jpg"
+  },
+  {
+    "revision": "78e585f6d495089f15e083ef739ad7e2",
+    "url": "/chapli-sante/static/media/img_bg_1.78e585f6.jpg"
+  },
+  {
+    "revision": "0dfe5dcad6ef318e7681a370a6a7203d",
+    "url": "/chapli-sante/static/media/img_bg_2.0dfe5dca.jpg"
+  },
+  {
+    "revision": "cfe97aec435ea6aa2be454e84e286fc2",
+    "url": "/chapli-sante/static/media/img_bg_3.cfe97aec.jpg"
+  },
+  {
+    "revision": "6499a839f595397f91b5c394f35d8974",
+    "url": "/chapli-sante/static/media/item-1.6499a839.jpg"
+  },
+  {
+    "revision": "fc82bc83f10ec4b855538da44f591e5b",
+    "url": "/chapli-sante/static/media/item-15.fc82bc83.jpg"
+  },
+  {
+    "revision": "3a12664d86013e405f80ef8188b1dc75",
+    "url": "/chapli-sante/static/media/item-2.3a12664d.jpg"
+  },
+  {
+    "revision": "785eb96b536a2ac45e0f0c2aaab851eb",
+    "url": "/chapli-sante/static/media/item-3.785eb96b.jpg"
+  },
+  {
+    "revision": "1a061f12c3b5da79c0494c9d15d84d25",
+    "url": "/chapli-sante/static/media/item-4.1a061f12.jpg"
+  },
+  {
+    "revision": "de955b62f6f87cd65847c1d7e1172f94",
+    "url": "/chapli-sante/static/media/item-6.de955b62.jpg"
+  },
+  {
+    "revision": "a8e5dad6c47a5a2863c59172bd2e4e68",
+    "url": "/chapli-sante/static/media/item-7.a8e5dad6.jpg"
+  },
+  {
+    "revision": "d99d8f99bfbf7f16a818112edb5ae22f",
+    "url": "/chapli-sante/static/media/item-9.d99d8f99.jpg"
+  },
+  {
+    "revision": "6499a839f595397f91b5c394f35d8974",
+    "url": "/chapli-sante/static/media/item_1.6499a839.jpg"
+  },
+  {
+    "revision": "36bc06367e8c5f0bc93965891770d629",
+    "url": "/chapli-sante/static/media/men.36bc0636.jpg"
+  },
+  {
+    "revision": "d6d8826a3ce8a76141f60389cf793f0e",
+    "url": "/chapli-sante/static/media/sports.d6d8826a.jpg"
+  },
+  {
+    "revision": "e67859ed63b3573ca35d1fa1396e194c",
+    "url": "/chapli-sante/static/media/women.e67859ed.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.226e2c789deed760bce182c2989ae935.js"
+  "/chapli-sante/precache-manifest.2505638a635571b2cab9edb0702fdca5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chapli-sante/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
