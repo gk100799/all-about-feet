0b1158cbb61198fc390a92f76a8cfe6952bb706c
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-05-30 01:43:26 +0530

    domain testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router as Router, Route, Switch, Redirect} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import Men from './components/homepage/Men'
 import Women from './components/homepage/Women'
 import About from './components/homepage/About'
